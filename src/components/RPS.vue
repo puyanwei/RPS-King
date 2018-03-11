c5f32e7ed2fef3be1ad3075ec74ef3ed1a3fcecc
Puyan Wei <puyanwei@gmail.com>
2018-03-11 13:31:21 +0000

    edited front page with description, name entry and submit button

--- src/components/RPS.vue
+++ src/components/RPS.vue
@@ -1,5 +1,14 @@
 <template>
-  <h1>RPS King</h1>
+  <div id="starting-page">
+    <h1>RPS King</h1>
+    <h3>You dare challenge me at Rock Paper Scissors?</h3>
+    <p>I have been a poker player for 20 years, you have no chance!</p>
+    <div slot="form-fields">
+        <input type="text" placeholder="name" required />
+        <button v-on:click="handleSubmit">Send</button>
+    </div>
+  </div>
+
 </template>
 
 <script>
