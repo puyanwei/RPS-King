5d45ae370f21c3b705d0724aa14aa976a66e8057
Puyan Wei <puyanwei@gmail.com>
2018-03-11 09:06:13 +0000

    created component RPS, linked to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <rock-paper-scissors></rock-paper-scissors>
   </div>
 </template>
 
 <script>
+import RPS from "./components/RPS.vue";
+
 export default {
-  name: 'App'
-}
+  components: {
+    "rock-paper-scissors": RPS
+  }
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/RPS.vue
@@ -0,0 +1,10 @@
+<template lang="html">
+  <h1>RPS King</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="css">
+</style>
